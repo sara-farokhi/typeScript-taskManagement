1205e0c55ca68d227e70fd2efbb8cd158024ebb3
sara-farokhi <sarafarokhi.sf@gmail.com>
2022-12-03 04:01:25 -0800

    create vallidation, item interface and todoItem

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,27 @@
 "use strict";
-let x = "hello";
+// get elements
+const form = document.getElementById("todo-form");
+const input = document.getElementById("title");
+const error = document.getElementById("title-error");
+class todoItem {
+    constructor(todo) {
+        this.title = todo.title;
+        this.id = todo.id;
+        this.status = false;
+    }
+}
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const todo = {
+        title: input.value.trim(),
+        id: Math.round(Math.random() * 100),
+        status: false,
+    };
+    let newTodo = new todoItem(todo);
+    if (newTodo.title.trim() === "") {
+        error.innerHTML = "required";
+    }
+    else {
+        error.innerHTML = "";
+    }
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,41 @@
-let x :string ="hello"
+// get elements
+
+const form = document.getElementById("todo-form")
+const input = document.getElementById("title") as HTMLInputElement
+const error = document.getElementById("title-error")
+interface item {
+    title: string;
+    id: number;
+    status: boolean
+}
+
+
+class todoItem implements item {
+    title: string
+    id: number
+    status: false
+
+    constructor(todo: item) {
+        this.title = todo.title
+        this.id = todo.id
+        this.status = false
+    }
+}
+
+
+
+form?.addEventListener("submit", (e: Event) => {
+    e.preventDefault()
+    const todo: item = {
+        title: input.value.trim(),
+        id: Math.round(Math.random() * 100),
+        status: false,
+    }
+    let newTodo = new todoItem(todo)
+    if (newTodo.title.trim() === "") {
+        error!.innerHTML = "required"
+    }
+    else {
+        error!.innerHTML = ""
+    }
+})
